69e85c17e96983c0e00cc307706c694d3ac24b3e
samandarwebdeveloper <86816538+samandarwebdeveloper@users.noreply.github.com>
2022-09-02 05:25:44 +0500

    Add response

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ ok: true, name: 'Samandar Xurramov' })
+  res.status(200).json({ ok: true, status: 200, name: 'Samandar Xurramov' })
 }
